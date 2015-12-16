b36fd591b7158efeaa4e2a206c7e550c9d40823e
Jeff Whitaker <jswhit@fastmail.fm>
2015-12-16 12:34:41 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from numpy.distutils.core  import setup, Extension
+
+# interface for Renka's algorithm 772 fortran code
+ext = Extension(name  = '_stripack',
+                sources       = ['_stripack.pyf','stripack.f90'])
+
+
+if __name__ == "__main__":
+    setup(name = 'stripack',
+          version           = "0.9",
+          description       = "Python interface to TOMS 772 (STRIPACK) fortran code",
+          author            = "Jeff Whitaker",
+          author_email      = "jeffrey.s.whitaker@noaa.gov",
+          ext_modules       = [ext],
+          packages          = ['stripack'],
+          )
---
+++ stripack/__init__.py
@@ -0,0 +1,76 @@
+import  _stripack
+import numpy as np
+
+class trmesh(object):
+    def __init__(self, lons, lats):
+        """
+given mesh points (lons,lats in radians) define triangulation.
+n is size of input mesh (length of 1-d arrays lons and lats).
+
+Algorithm:
+ R. J. Renka, "ALGORITHM 772: STRIPACK: Delaunay triangulation
+ and Voronoi diagram on the surface of a sphere"
+ ACM Trans. Math. Software, Volume 23 Issue 3, Sept. 1997
+ pp 416-434."""
+        if len(lons.shape) != 1 or len(lats.shape) != 1:
+            raise ValueError('lons and lats must be 1d')
+        lats = lats.astype(np.float64,copy=False)
+        lons = lons.astype(np.float64,copy=False)
+        if (np.abs(lons)).max() > 2.*np.pi:
+            msg="lons must be in radians (-2*pi <= lon <= 2*pi)"
+            raise ValueError(msg)
+        if (np.abs(lats)).max() > 0.5*np.pi:
+            msg="lats must be in radians (-pi/2 <= lat <= pi/2)"
+            raise ValueError(msg)
+        npts = len(lons)
+        if len(lats) != npts:
+            raise ValueError('lons and lats must have same length')
+        # compute cartesian coords on unit sphere.
+        x,y,z = _stripack.trans(lats,lons,npts)
+        lst,lptr,lend,ierr = _stripack.trmesh(x,y,z,npts)
+        if ierr != 0:
+            raise ValueError('ierr = %s in trmesh' % ierr)
+        self.lons = lons; self.lats = lats; self.npts = npts
+        self.x = x; self.y = y; self.z = z
+        self.lptr = lptr; self.lst = lst; self.lend = lend
+    def interp(self,olons,olats,data,order=1):
+        """
+given a triangulation, perform interpolation on
+oints olons,olats (in radians), return result in data.
+olons, olats can be 1d or 2d (output data array has same shape as olats,lons).
+order of interpolation specified by 'order' kwarg, can be 0 (nearest neighbor),
+or 1 (linear)."""
+        shapeout = olons.shape
+        if len(shapeout) not in [1,2]:
+            raise ValueError('olons,olats must be 1d or 2d')
+        olons1 = (olons.astype(np.float64,copy=False)).ravel()
+        olats1 = (olats.astype(np.float64,copy=False)).ravel()
+        nptso = len(olons1)
+        if (np.abs(olons1)).max() > 2.*np.pi:
+            msg="lons must be in radians (-2*pi <= lon <= 2*pi)"
+            raise ValueError(msg)
+        if (np.abs(olats1)).max() > 0.5*np.pi:
+            msg="lats must be in radians (-pi/2 <= lat <= pi/2)"
+            raise ValueError(msg)
+        if len(olats1) != nptso:
+            raise ValueError('lons and lats must have same length')
+        if len(data) != self.npts:
+            raise ValueError('input data wrong size')
+        if order not in [0,1]:
+            raise ValueError('order must be 0,1')
+        else:
+            odata,ierr = \
+            _stripack.interp_n(order, olats1, olons1,\
+            self.x, self.y, self.z, data.astype(np.float64),\
+            self.lst,self.lptr,self.lend,self.npts,nptso)
+        if ierr != 0:
+            raise ValueError('ierr = %s in intrpc0_n' % ierr)
+        return odata.reshape(shapeout)
+    def interp_nn(self,olons,olats,data):
+        """
+same as interp(olons,olats,data,order=0)"""
+        return self.interp(olons,olats,data,order=0)
+    def interp_linear(self,olons,olats,data):
+        """
+same as interp(olons,olats,data,order=1)"""
+        return self.interp(olons,olats,data,order=1)
