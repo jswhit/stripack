da516c42f53324066b060286881ad96e1d440f5c
Jeff Whitaker <jswhit@fastmail.fm>
2015-12-20 14:09:29 -0700

    put description, email and download urls in

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ ext = Extension(name  = '_stripack',
 
 if __name__ == "__main__":
     setup(name = 'stripack',
-          version           = "1.0",
-          description       = "Python interface to TOMS 772 (STRIPACK) fortran code",
           author            = "Jeff Whitaker",
           author_email      = "jeffrey.s.whitaker@noaa.gov",
+          url               = "https://github.com/jswhit/stripack",
+          download_url      = "http://python.org/pypi/stripack",
+          version           = "1.0",
+          description       = "Python interface to TOMS 772 (STRIPACK) fortran code",
           ext_modules       = [ext],
           packages          = ['stripack'],
           )
