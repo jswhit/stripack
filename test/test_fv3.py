2e180655e3fd76366c7dff0720fe428dc7c5834b
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2018-02-10 11:56:25 -0500

    update

--- test/test_fv3.py
+++ test/test_fv3.py
@@ -6,54 +6,23 @@ import cPickle
 
 # test fv3 interpolation from native history files to random points.
 
-res = 96  
+res = 128 
 fixfv3 = '/scratch4/NCEPDEV/global/save/glopara/svn/fv3gfs/fix_fv3'
+#fixfv3='/lustre/f1/unswept/Jeffrey.S.Whitaker/fv3_reanl/fv3gfs/global_shared.v15.0.0/fix/fix_fv3_gmted2010'
 datapath = '/scratch3/BMC/gsienkf/whitaker/fv3_ics64'
+#datapath = '/lustre/f1/unswept/Jeffrey.S.Whitaker/fv3_reanl/ics'
 date = '2016100100'
 var = 'pressfc'
 ntime = 0
 
-# test computation of cell centers given cell corners.
-#from spherical_geometry import vector, great_circle_arc
-#for ntile in range(1,7,1):
-#    gridspecfile = '%s/C%s/C%s_grid_spec.tile%s.nc'% (fixfv3,res,res,ntile)
-#    print 'reading ',gridspecfile
-#    nc = Dataset(gridspecfile)
-#    lonscorner = nc['grid_lon'][:]
-#    latscorner = nc['grid_lat'][:]
-#    latsA = latscorner[:-1,:-1].ravel(); lonsA = lonscorner[:-1,:-1].ravel() # lower left
-#    latsC = latscorner[1:,:-1].ravel(); lonsC = lonscorner[1:,:-1].ravel() # upper left
-#    latsB = latscorner[1:,1:].ravel(); lonsB = lonscorner[1:,1:].ravel() # upper right
-#    latsD = latscorner[:-1,1:].ravel(); lonsD = lonscorner[:-1,1:].ravel() # lower right
-#    nxp1,nyp1 = lonscorner.shape; nx = nxp1-1; ny = nyp1-1
-#    npts = len(latsA)
-#    A = np.empty((npts,3),dtype=np.float)
-#    B = np.empty((npts,3),dtype=np.float)
-#    C = np.empty((npts,3),dtype=np.float)
-#    D = np.empty((npts,3),dtype=np.float)
-#    A[:,0],A[:,1],A[:,2] = vector.radec_to_vector(lonsA, latsA, degrees=True)
-#    B[:,0],B[:,1],B[:,2] = vector.radec_to_vector(lonsB, latsB, degrees=True)
-#    C[:,0],C[:,1],C[:,2] = vector.radec_to_vector(lonsC, latsC, degrees=True)
-#    D[:,0],D[:,1],D[:,2] = vector.radec_to_vector(lonsD, latsD, degrees=True)
-#    # find intersection of great circle arcs from each corner of grid box
-#    T = great_circle_arc.intersection(A, B, C, D)
-#    lonsmid, latsmid =\
-#    vector.vector_to_radec(T[:,0],T[:,1],T[:,2],degrees=True)
-#    lonsmid = lonsmid.reshape((ny,nx))
-#    latsmid = latsmid.reshape((ny,nx))
-#    lonsmid2 = nc['grid_lont'][:]
-#    latsmid2 = nc['grid_latt'][:]
-#    difflon = lonsmid-lonsmid2; difflat = latsmid-latsmid2
-#    print 'lon error min/max',difflon.min(), difflon.max()
-#    print 'lat error min/max',difflat.min(), difflat.max()
 
 # perform triangulation.
 lons = []; lats = []
 for ntile in range(1,7,1):
-    gridspecfile = '%s/C%s/C%s_grid_spec.tile%s.nc'% (fixfv3,res,res,ntile)
-    nc = Dataset(gridspecfile)
-    lonsmid = nc['grid_lont'][:]
-    latsmid = nc['grid_latt'][:]
+    gridfile = '%s/C%s/C%s_grid.tile%s.nc'% (fixfv3,res,res,ntile)
+    nc = Dataset(gridfile)
+    lonsmid = nc['x'][1::2,1::2]
+    latsmid = nc['y'][1::2,1::2]
     lons.append(lonsmid); lats.append(latsmid)
     nc.close()
 lons = np.radians(np.array(lons,dtype=np.float64)).ravel()
