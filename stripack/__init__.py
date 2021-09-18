877146b5a28c0b11d55037b44154e9d510bc0ffd
Jeff Whitaker <jswhit@fastmail.fm>
2021-09-18 15:47:20 -0600

    fix typo

--- stripack/__init__.py
+++ stripack/__init__.py
@@ -131,14 +131,14 @@ quiet: Suppress diagnostic messages (default False)
 
 returns lons2d,lats2d,latlon_data where
 lons2d: 2d array of longitudes on output grid (in degrees)
-lats2d: 2d array of longitudes on output grid (in degrees)
+lats2d: 2d array of latiitudes on output grid (in degrees)
 latlon_data:  2d array of interpolated data on output grid.
         """
         # generate regular 2d lat/lon grid (not including poles,
         # or wrap-around longitude).
-        nlats = nlons/2
-        olons = (360./nlons)*np.arange(nlons)
-        olats = -90 + 0.5*(360./nlons) + (360./nlons)*np.arange(nlats)
+        nlats = nlons/2; delta = 360./nlons
+        olons = delta*np.arange(nlons)
+        olats = -90. + delta*(0.5 + np.arange(nlats))
         olonsd, olatsd = np.meshgrid(olons, olats) # degrees
         # interpolate to the reg lat/lon grid
         if not quiet: t1 = time.time()
