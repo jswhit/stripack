b28564b18bdb1ee2d0c0573472fedfafdabfd3fe
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2021-09-18 15:25:34 +0000

    batch script

---
+++ fv3/generate_fv3mesh.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --ntasks-per-node=40 --nodes=1
+#SBATCH -t 8:00:00
+#SBATCH -A gsienkf
+##SBATCH -q debug
+#SBATCH -J generate_mesh
+#SBATCH -o generate_mesh.out
+#SBATCH -e generate_mesh.err
+source ~/bin/condapy
+python generate_fv3mesh.py 48 &
+python generate_fv3mesh.py 96 &
+python generate_fv3mesh.py 192 &
+python generate_fv3mesh.py 384 &
+python generate_fv3mesh.py 768 &
+python generate_fv3mesh.py 1152 &
+python generate_fv3mesh.py 3072 &
+wait
