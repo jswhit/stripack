c7d3a1b8a80f4e955795d3941536a3cc1e6abac9
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2021-09-18 15:24:48 +0000

    update

--- fv3/test_interp.py
+++ fv3/test_interp.py
@@ -14,6 +14,7 @@ else:
 fixfv3 = '/scratch1/NCEPDEV/global/glopara/fix_NEW/fix_fv3_gmted2010/'
 
 # perform interpolation of orography using saved triangulation.
+# read data from each of the 6 tile files, append into 1d array
 data = []
 for ntile in range(1,7,1):
     gridfile = '%s/C%s/C%s_oro_data.tile%s.nc'% (fixfv3,res,res,ntile)
@@ -31,16 +32,16 @@ tri = cPickle.load(open(picklefile,'rb'))
 nlons = 1440; nlats = nlons/2 
 olons = (360./nlons)*np.arange(nlons)
 olats = -90 + 0.5*(360./nlons) + (360./nlons)*np.arange(nlats)
-olonsd, olatsd = np.meshgrid(olons, olats)
-olons = np.radians(olons);  olats = np.radians(olats)
+olonsd, olatsd = np.meshgrid(olons, olats) # degrees
+olons = np.radians(olons);  olats = np.radians(olats) # radians
 olons, olats = np.meshgrid(olons, olats)
 
 # interpolate linearly to the reg grid
 # interp_cubic and interp_nn (nearest neighbor) also available
 t1 = time.time()
 if tri._shuffle:
-    data = data[tri._ix] # points were randomly shuffled
-latlon_data = tri.interp_linear(olons,olats,data)
+    data = data[tri._ix] # points were randomly shuffled to make triangulation faster
+latlon_data = tri.interp_linear(olons,olats,data) # expects radians
 print('time to interpolation =',time.time()-t1)
 print(latlon_data.shape, latlon_data.min(), latlon_data.max())
 
